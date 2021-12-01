3e43e08e1dcfe76d7995224f11c2e35a62e277dc
eneko98 <enekovalero@gmail.com>
2021-12-01 20:45:10 +0100

    ultrasonic_ranger import

--- conversion.py
+++ conversion.py
@@ -1,15 +1,15 @@
 import time
-from grove.grove_ultrasonic_ranger import GroveUltrasonicRanger
+from grove import GroveUltrasonicRanger
 def main():
     sensor = GroveUltrasonicRanger(16)
     while True: #mientras no se de el paro del sistema (cambiar)
-        distance = sensor.get_distance() / 100
+        distance = sensor.get_distance()/100
         print(distance)
         if distance < 1.50:
             print('Cerca')
         else:
             print('Lejos')
-    time.sleep(1)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
