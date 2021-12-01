07616b93ed35f3d0d8ccf662ccc443c93a2b509d
eneko98 <enekovalero@gmail.com>
2021-12-01 18:06:04 +0100

    Primer commit

---
+++ ultrasonidos.py
@@ -0,0 +1,17 @@
+import time
+from grove.grove_ultrasonic_ranger import GroveUltrasonicRanger
+def main():
+ # Grove - Ultrasonic Ranger connected to port D16
+    sensor = GroveUltrasonicRanger(16)
+    while True:
+        distance = sensor.get_distance()
+        print('{} cm'.format(distance))
+        if distance < 100:
+            print('Cerca')
+        else:
+            print('Lejos')
+    time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
