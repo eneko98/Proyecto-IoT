b348fed934f995687fd3c5d8368bc22c0e3a2288
eneko98 <enekovalero@gmail.com>
2021-12-07 18:04:03 +0100

    index y detalle actualizada

--- SensorUI/SensorApp/models.py
+++ SensorUI/SensorApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from decimal import Decimal
 
 class UltrasonicSensor(models.Model):
     """
@@ -7,6 +8,5 @@ class UltrasonicSensor(models.Model):
     name = models.CharField(max_length=50, default="HC-SR04")
     description = models.TextField()
     pin = models.IntegerField()
-    distance = models.BigIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    distance = models.DecimalField(max_digits=20, decimal_places=4)
+    date = models.DateTimeField(auto_now_add=True)
--- SensorUI/SensorApp/views.py
+++ SensorUI/SensorApp/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import UltrasonicSensor
-from Sensores import ultrasonidos
+#from Sensores import ultrasonidos
 
 class IndexView(ListView):
     template_name = 'index.html'
     context_object_name = 'sensors'
 
     def get_queryset(self):
-        return UltrasonicSensor.objects.order_by('-id')[:5]
+        return UltrasonicSensor.objects.all()
 
-class SensorDetailView(DetailView):
+class uSensorDetailView(DetailView):
     template_name = 'sensor_detail.html'
     context_object_name = 'sensor'
+
+    def get_queryset(self):
+        return UltrasonicSensor.objects.all()
