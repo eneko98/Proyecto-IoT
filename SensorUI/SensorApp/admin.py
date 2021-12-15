fbb321d52b6a3ebaccd0332ef0cd79bf6b75d22b
eneko98 <enekovalero@gmail.com>
2021-12-15 18:05:56 +0100

    readonly field in django admin

--- SensorUI/SensorApp/admin.py
+++ SensorUI/SensorApp/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import UltrasonicSensor
 
+class USAdmin(admin.ModelAdmin):
+    readonly_fields = ('date',)
+
+admin.site.register(UltrasonicSensor, USAdmin)
 admin.site.register(UltrasonicSensor)
