4e294c9865ffa49c2eeda9d2f0d963786e2c407a
eneko98 <enekovalero@gmail.com>
2021-12-31 19:19:53 +0100

    face recognition finished

--- SensorUI/SensorApp/face_detection.py
+++ SensorUI/SensorApp/face_detection.py
@@ -1,7 +1,19 @@
 import numpy as np
 import cv2
+import imutils
+from PIL import Image
+import os
+import pickle
 
-faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+faceDetector = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+eyeDetector = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+labels = {"person_name": 1}
+with open("labels.pickle", "rb") as f:
+    og_label = pickle.load(f)
+    label = {v:k for k,v in og_label.items()}
 
 class VideoCamera(object):
     def __init__(self):
@@ -12,16 +24,20 @@ class VideoCamera(object):
     def get_frame(self):
         ret,image = self.video.read()
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
+        faces = faceDetector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
         for (x,y,w,h) in faces:
+            id_, conf = recognizer.predict(gray[y:y+h, x:x+w])
+            if conf >=45:
+                #print(id_)
+                #print(label[id_])
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                name = label[id_]
+                color = (255, 255, 255)
+                stroke = 2
+                cv2.putText(image, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
             cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
-        frame_flip = cv2.flip(image, 1)
-        ret,jpeg = cv2.imencode('.jpg', frame_flip)
+        eyes = eyeDetector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
+        for (x,y,w,h) in eyes:
+            cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+        ret,jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
-
-
-
-
-
-
-        
